bc43a63038b6ca66ef3c84d268322fbee710b3a2
dozorovala <dozorovala@sibur.ru>
2023-12-13 13:15:41 +0300

    create application

---
+++ src/main/java/ru/test/EffecticeMobileTestWork/EffecticeMobileTestWorkApplication.java
@@ -0,0 +1,13 @@
+package ru.test.EffecticeMobileTestWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffecticeMobileTestWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffecticeMobileTestWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/test/EffecticeMobileTestWork/EffecticeMobileTestWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.test.EffecticeMobileTestWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffecticeMobileTestWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
